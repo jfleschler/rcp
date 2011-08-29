8af696859229480112afd30c3b1e0caf5ac07000
Jon Fleschler <jon.fleschler@gmail.com>
2011-08-29 13:59:34 -0500

    switch to bootstrap css

--- app/models/step.rb
+++ app/models/step.rb
@@ -2,4 +2,30 @@ class Step < ActiveRecord::Base
 	belongs_to :recipe
 	has_many :associations, :dependent => :destroy
 	has_many :ingredients, :through => :associations
+
+	def number_string
+		case step_num
+		when 1
+			"q"
+		when 2
+			"w"
+		when 3
+			"e"
+		when 4
+			"r"
+		when 5
+			"t"
+		when 6
+			"y"
+		when 7
+			"u"
+		when 8
+			"i"
+		when 9
+			"o"
+		when 0
+			"p"
+		end
+	end
+
 end
