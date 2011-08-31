2aef09240636b9a55eadb82fd199956ad8bd3404
Jon Fleschler <jon.fleschler@gmail.com>
2011-08-31 16:02:57 -0500

    swappable ingredients

--- app/controllers/associations_controller.rb
+++ app/controllers/associations_controller.rb
@@ -4,9 +4,38 @@ class AssociationsController < ApplicationController
   	@recipe = Recipe.find(params[:associations][:recipe])
   	step = Step.find(params[:associations][:activeStep])
   	step.associations.create({:ingredient_id => params[:associations][:selectedIngredient],
-	  						  :amount        => "#{params[:associations][:amount]} #{params[:associations][:amount_unit]}"})
+	  						  :amount        => "#{params[:associations][:amount]} #{params[:associations][:amount_unit]}",
+	  						  :position      => step.associations.count + 1})
 
   	respond_to do |format|
+	    format.html { redirect_to edit_recipe_path(@recipe) }
+	    format.js
+    end
+  end
+
+  def update
+  	association = Association.find(params[:id])
+	step = Step.find(association.step_id)
+  	@recipe = Recipe.find(step.recipe_id)
+
+  	current_pos = association.position
+
+  	if(params[:move] == 'left')
+  		swap_with = step.associations.find_by_position(current_pos - 1)
+  		swap_with.position += 1;
+  		swap_with.save
+  		association.position -= 1;
+		association.save
+
+  	elsif (params[:move] == 'right')
+  	  	swap_with = step.associations.find_by_position(current_pos + 1)
+  		swap_with.position -= 1;
+  		swap_with.save
+  		association.position += 1;
+		association.save
+  	end
+	
+	respond_to do |format|
 	    format.html { redirect_to edit_recipe_path(@recipe) }
 	    format.js
     end
--- app/models/association.rb
+++ app/models/association.rb
@@ -2,8 +2,23 @@ class Association < ActiveRecord::Base
 	belongs_to :ingredient
 	belongs_to :step
 
+	before_destroy :renumber_remaining
+
+	default_scope :order => 'position'
+
 	def ingredient
 		Ingredient.find(ingredient_id)
 	end
 
+	private
+
+		def renumber_remaining
+			step = Step.find(step_id)
+			step.associations.each do |a|
+				if(a.position > position)
+					a.position = a.position - 1
+					a.save
+				end
+			end
+		end
 end
