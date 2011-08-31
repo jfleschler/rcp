d167ba0ec4dde62edf957bbd242caa28aaa79071
Jon Fleschler <jon.fleschler@gmail.com>
2011-08-31 11:48:05 -0500

    gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rcp::Application.routes.draw do
   resources :associations
 
   root :to => 'recipes#index'
+
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
