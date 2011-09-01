4a02615400da278069af6d65e0bd59867e4e388e
Jon Fleschler <jon.fleschler@gmail.com>
2011-09-01 16:46:49 -0500

    ingredient list started

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,8 @@
 class Recipe < ActiveRecord::Base
 	has_many :steps, :dependent => :destroy
-
+	has_many :associations, :through => :steps
+	has_many :ingredients,  :through => :associations
+	
 	def step_attributes=(step_attributes)
 	  step_attributes.each do |attributes|
 	    steps.build(attributes)
