f03a6425fb01dbe052035239564012a5bcf10381
Jon Fleschler <jon.fleschler@gmail.com>
2011-09-02 13:24:22 -0500

    fixed styles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,29 +5,20 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
-ingredients = Ingredient.create([
-	{:name => "butter",
-	 :image => "placeholder.png"},
-	{:name => "white sugar",
-	 :image => "placeholder.png"},
-	{:name => "brown sugar",
-	 :image => "placeholder.png"},
-	{:name => "egg",
-	 :image => "placeholder.png"},
-	{:name => "vanilla extract",
-	 :image => "placeholder.png"},
-	{:name => "flour",
-	 :image => "placeholder.png"},
-	{:name => "salt",
-	 :image => "placeholder.png"},
-	{:name => "baking powder",
-	 :image => "placeholder.png"},
-	{:name => "baking soda",
-	 :image => "placeholder.png"},
-	{:name => "chocolate chips",
-	 :image => "placeholder.png"},
-	{:name => "cinnamon",
-	 :image => "placeholder.png"},
-	{:name => "milk",
-	 :image => "placeholder.png"}
-])
+ingredients = [
+	{:name => "butter",				:image => "placeholder.png"},
+	{:name => "white sugar",		:image => "placeholder.png"},
+	{:name => "brown sugar",		:image => "placeholder.png"},
+	{:name => "egg",				:image => "placeholder.png"},
+	{:name => "vanilla extract",	:image => "placeholder.png"},
+	{:name => "flour",				:image => "placeholder.png"},
+	{:name => "salt",				:image => "placeholder.png"},
+	{:name => "baking powder",		:image => "placeholder.png"},
+	{:name => "baking soda",		:image => "placeholder.png"},
+	{:name => "chocolate chips",	:image => "placeholder.png"},
+	{:name => "cinnamon",			:image => "placeholder.png"},
+	{:name => "milk",				:image => "placeholder.png"},
+	{:name => "raisins",			:image => "placeholder.png"}
+].each do |i|
+	Ingredient.find_or_create_by_name(i)
+end
