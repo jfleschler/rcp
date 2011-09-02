c3c925fecb6793573dd64923cbc304e2de64daf8
Jon Fleschler <jon.fleschler@gmail.com>
2011-09-02 15:25:19 -0500

    public

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -8,11 +8,15 @@ class Recipe < ActiveRecord::Base
 	validates :user_id, :presence => true
 	validates :name, :presence => true
 
-	scope :public, where(:public => true)
+	#scope :public, where(:public => 1)
 	
 	def step_attributes=(step_attributes)
 	  step_attributes.each do |attributes|
 	    steps.build(attributes)
 	  end
+	end
+
+	def self.public
+		Recipe.where(:public => 1)
 	end
 end
