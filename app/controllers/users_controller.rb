76f741a12e443f0fb1c0025fd8021edf50d29396
Jon Fleschler <jon.fleschler@gmail.com>
2011-09-02 15:00:12 -0500

    fixed index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(params[:user])
 		if @user.save
+			sign_in @user
 			redirect_to root_url, :notice => "user has been created!"
 		else
 			render "new"
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -8,7 +8,7 @@ class Recipe < ActiveRecord::Base
 	validates :user_id, :presence => true
 	validates :name, :presence => true
 
-	scope :public, where(:public => 1)
+	scope :public, lambda { where(:public => 1) }
 	
 	def step_attributes=(step_attributes)
 	  step_attributes.each do |attributes|
