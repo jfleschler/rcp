222c1ba584c5ce5160121cac68968eba8f69f2e3
Jon Fleschler <jon.fleschler@gmail.com>
2011-09-15 14:58:04 -0500

    destroying user now destroys their rcps

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
 	validates :username, :uniqueness => true, :presence => true
 	validates :email,    :uniqueness => true, :presence => true
 
-	has_many :recipes
+	has_many :recipes, :dependent => :destroy
 
 	before_save :generate_salt
 
